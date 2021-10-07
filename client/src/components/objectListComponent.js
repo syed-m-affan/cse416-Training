f12e6ca8a055560e1d8d469b1203d859e6cb8341
Syed Affan <saffan98@gmail.com>
2021-10-06 20:30:52 -0400

    typo

--- client/src/components/objectListComponent.js
+++ client/src/components/objectListComponent.js
@@ -19,7 +19,7 @@ export default class ObjectList extends Component {
     }
 
     componentDidMount(){
-        axios.get('localhost:4000/objects')
+        axios.get('localhost:4000/objects/')
             .then(response => {
                 this.setState({objects: response.data});
             })
