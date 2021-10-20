4227354f2660196f2f28a76a8be498d11b15bb64
Syed Affan <saffan98@gmail.com>
2021-10-20 18:33:40 -0400

    test profile 2

--- client/src/components/objectViewComponent.js
+++ client/src/components/objectViewComponent.js
@@ -53,68 +53,215 @@ export default class ViewObject extends Component {
 
     render() {
         return(
-            <div style={{background: '#FF5353'}}>
-                <Container>
+            <div
+    style={{
+      background: "#FF5353",
+      width: "1920px",
+      height: "1080px",
+      position: "absolute",
+      left: "0px",
+      top: "0px"
+    }}
+  >
+    <Button
+      style={{
+        position: "absolute",
+        width: "244px",
+        height: "69px",
+        left: "18px",
+        top: "28px",
+        background: "#C4C4C4",
+        border: "solid",
+        borderColor: "#000000",
+        borderSize: "2px",
+        boxSizing: "border-box",
+        borderRadius: "15px"
+      }}
+      to={"/"}
+    >
+      <div
+        style={{
+          position: "absolute",
+          width: "205px",
+          height: "41px",
+          left: "19px",
+          top: "11px",
 
-                    <Row>
+          fontFamily: "Roboto",
+          fontStyle: "normal",
+          fontWeight: "normal",
+          fontSize: "35px",
+          lineHeight: "41px",
+          color: "#000000"
+        }}
+      >
+        Return Home
+      </div>
+    </Button>
 
-                        <Link to={'/home'}>Home</Link>
+    <div
+      style={{
+        background: "gray",
+        position: "absolute",
+        width: "1361px",
+        height: "610px",
+        left: "279px",
+        top: "8px",
 
-                    </Row>
-                    <Row>
-                        <Stack direction="horizontal" gap={3}>
-                            <div style={{background: '#87CEEB',
-                                        borderRadius: '25px'}}>
-                                <Stack gap={2}>
-                                    <div>
-                                        <Stack direction="horizontal" gap={3}>
-                                            <div style={{background: '#FFA2A2',
-                                                        borderRadius: '15px',
-                                                        }}>Your Platforms</div>
-                                            <div style={{background: '#FFA2A2',
-                                                        borderRadius: '15px',
-                                                        }}>Create New Platform</div>
-                                        </Stack>
-                                    </div>
-                                    <div style={{background: '#FFA2A2',
-                                                        borderRadius: '15px',
-                                                        }}>
-                                        Baseball101
-                                    </div>
-                                    <div style={{background: '#FFA2A2',
-                                                        borderRadius: '15px',
-                                                        }}>
-                                        Racecar Fans
-                                    </div>
-                                </Stack>
+        border: "solid",
+        borderSize: "4px",
+        borderRadius: "25px",
+        boxSizing: "border-box"
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          width: "511px",
+          height: "511px",
+          left: "424px",
+          top: "8px",
 
-                            </div>
-                            <div style={{background: '#87CEEB',
-                                        borderRadius: '25px'}}>
-                                <Stack gap={2}>
-                                    <div style={{background: '#FFA2A2',
-                                                        borderRadius: '15px',
-                                                        }}>
-                                        Recently
-                                        
-                                    </div>
-                                    <div style={{background: '#FFA2A2',
-                                                        borderRadius: '15px',
-                                                        }}>
-                                        Baseball101
-                                    </div>
-                                    <div style={{background: '#FFA2A2',
-                                                        borderRadius: '15px',
-                                                        }}>
-                                        Racecar Fan
-                                    </div>
-                                </Stack>
+          background: "#AAAAAA",
+          border: "solid",
+          boxSizing: "border-box",
+          borderRadius: "50%"
+        }}
+      ></div>
 
-                            </div>
-                        </Stack>
-                    </Row>
-                </Container>
-            </div>
+      <div
+        style={{
+          position: "absolute",
+          width: "272px",
+          height: "54px",
+          left: "1064px",
+          top: "437px",
+
+          background: "#FFA2A2",
+          border: "solid",
+          boxSizing: "border-box",
+          borderRadius: "15px"
+        }}
+      ></div>
+
+      <div
+        style={{
+          position: "absolute",
+          width: "272px",
+          height: "54px",
+          left: "1064px",
+          top: "519px",
+
+          background: "#FFA2A2",
+          border: "solid",
+          boxSizing: "border-box",
+          borderRadius: "15px"
+        }}
+      ></div>
+
+      <div
+        style={{
+          position: "absolute",
+          width: "288px",
+          height: "68px",
+          left: "534.5px",
+          top: "531px",
+
+          background: "#87CEEB",
+          border: "solid",
+          borderRadius: "15px"
+        }}
+      ></div>
+    </div>
+
+    <div
+      style={{
+        position: "absolute",
+        width: "843px",
+        height: "478px",
+        left: "78px",
+        top: "634px",
+
+        background: "#87CEEB",
+        boxSizing: "border-box",
+        borderRadius: "25px",
+        border: "solid",
+        borderSize: "4px"
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          width: "773px",
+          height: "120px",
+          left: "35px",
+          top: "169px",
+
+          background: "#FFA2A2",
+          border: "solid",
+          borderRadius: "15px"
+        }}
+      ></div>
+
+      <div
+        style={{
+          position: "absolute",
+          width: "773px",
+          height: "120px",
+          left: "35px",
+          top: "321px",
+
+          background: "#FFA2A2",
+          border: "solid",
+          borderRadius: "15px"
+        }}
+      ></div>
+    </div>
+
+    <div
+      style={{
+        position: "absolute",
+        width: "843px",
+        height: "478px",
+        left: "1000px",
+        top: "634px",
+
+        background: "#87CEEB",
+        boxSizing: "border-box",
+        borderRadius: "25px",
+        border: "solid",
+        borderSize: "4px"
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          width: "773px",
+          height: "120px",
+          left: "35px",
+          top: "169px",
+
+          background: "#FFA2A2",
+          border: "solid",
+          borderRadius: "15px"
+        }}
+      ></div>
+
+      <div
+        style={{
+          position: "absolute",
+          width: "773px",
+          height: "120px",
+          left: "35px",
+          top: "321px",
+
+          background: "#FFA2A2",
+          border: "solid",
+          borderRadius: "15px"
+        }}
+      ></div>
+    </div>
+  </div>
         )
     }
 }
