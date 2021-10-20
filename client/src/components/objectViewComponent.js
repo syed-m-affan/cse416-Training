670e8e0c323f79e75a9bd1598bab9ddaf1139b07
Syed Affan <saffan98@gmail.com>
2021-10-20 11:49:56 -0400

    testing

--- client/src/components/objectViewComponent.js
+++ client/src/components/objectViewComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button'
 
 export default class ViewObject extends Component {
 
@@ -50,17 +51,29 @@ export default class ViewObject extends Component {
 
     render() {
         return(
-            <div style={{marginTop: 10,
-                textAlign: "center"}}>
-                <h2>View Object</h2>
-                <div style={{color: this.state.objectColor,
-                            textAlign: "center",
-                            fontSize: this.state.objectSize + "px"}}>{this.state.objectText}</div>
-                <Link to={"/edit/"+this.props.match.params.id} className="btn btn-primary">Edit</Link>
-                <Link to={"/"} className="btn btn-warning">Exit</Link>
-                <button className="btn btn-danger" onClick={this.onDelete}>Delete Object</button>
+            <div style={{background: '#FF5353'}}>
+
+                <div style= {{  position: 'absolute',
+                                width: '244px',
+                                height: '69px',
+                                left: '18px',
+                                top: '28px',
+                                background: '#C4C4C4',
+                                border: '2px',
+                                boxSizing: 'border-box',
+                                borderRadius: '15px'}}>Return Home</div>
+
+                <div style= {{ background: "gray",
+                                position: 'absolute',
+                                width: '1361px',
+                                height: '610px',
+                                left: '279px',
+                                top: '8px',
+                                border: '4px',
+                                borderRadius: '25px',
+                                boxSizing: 'border-box'
+                                }}></div>
             </div>
-            
         )
     }
 }
